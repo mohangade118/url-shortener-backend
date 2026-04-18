8f7df2fdba881cc829964b9de238891ecb0e5276
mohangade118 <mohangade118@gmai..com>
2026-04-18 08:52:05 +0000

    setting up project.

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,9 @@
 <?php
 
-print "Project base setup done....";
-print "Project base setup done....";
-print "Project base setup done....";
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return static function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
---
+++ backend/src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'main')]
+    public function home()
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+}
